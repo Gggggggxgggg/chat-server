8977bceabb72deaa698647df49f0235911388820
Gggggggxgggg <kslanzo52@gmail.com>
2026-06-29 19:54:02 +0600

    Refactor WebSocket message broadcasting and logging

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const WebSocket = require("ws");
 
 const PORT = process.env.PORT || 8080;
+
 const server = new WebSocket.Server({ port: PORT });
 
 server.on("connection", (ws) => {
+
     ws.on("message", (msg) => {
-        server.clients.forEach((client) => {
+
+        server.clients.forEach(client => {
+
             if (client.readyState === WebSocket.OPEN) {
                 client.send(msg.toString());
             }
+
         });
+
     });
+
 });
 
-console.log("Server started");
+console.log("Chat Server Started on port " + PORT);
