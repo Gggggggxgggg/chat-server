22422f4f5b86c105abeb66dc729f0e3976e02cd9
Gggggggxgggg <kslanzo52@gmail.com>
2026-06-29 19:41:34 +0600

    Implement WebSocket server in server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const WebSocket = require("ws");
+
+const PORT = process.env.PORT || 8080;
+const server = new WebSocket.Server({ port: PORT });
+
+server.on("connection", (ws) => {
+    ws.on("message", (msg) => {
+        server.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(msg.toString());
+            }
+        });
+    });
+});
+
+console.log("Server started");
